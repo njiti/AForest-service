e5a3a32fcaa9d396f1171c88f9a1122e3966dcf4
njiti <kamaukan77@gmail.com>
2022-01-18 12:37:56 -0930

    Corrected postgre password

--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -4,9 +4,9 @@ import org.sql2o.*;
 
 public class DB {
 
-    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-158-232-223.compute-1.amazonaws.com:5432/d74l98ruq4hcnn?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", "administrator", "2541kamau");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-54-158-232-223.compute-1.amazonaws.com:5432/d74l98ruq4hcnn?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", "postgres", "2541kamau");
 
 
-//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "administrator", "2541kamau");
+//    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "postgres", "2541kamau");
 
 }
